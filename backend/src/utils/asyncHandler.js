aef4127dbf52a55454fc7deb4384f177abce3ccd
Bhavesh-Solminde <bhaveshsolminde@gmail.com>
2025-12-16 04:43:41 +0530

    utils created

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (err) {
+    res.status(err.status || 500).json({
+      success: false,
+      message: err.message || "Internal Server Error",
+    });
+  }
+};
+
+export default asyncHandler;
+
+// Alternative implementation using Promise
+// const asyncHandler = (fn) => { async (req, res, next) => {
+//     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+// }}
